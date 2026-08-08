90695dba3b9c9a7f1be2df70e653cd925f3c6ff0
jeremypanaligan-cmd <jeremy.panaligan@gllopvision.com>
2026-08-09 00:54:09 +0800

    TRY ULIT PARA SA SORT

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
+const CACHE_VERSION = 'gvsi-v3';
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  // Piliting mag-fetch sa network nang walang HTTP cache
+  // Piliting mag-fetch sa network nang direktang walang HTTP cache
   e.respondWith(
     fetch(e.request, { cache: 'no-store' }).catch(() => fetch(e.request))
   );
